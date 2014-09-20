5a69345d4aa81f01f324190121245969bb1cb3ed
Francisco Maria Calisto <francisco.calisto@tecnico.ulisboa.pt>
2014-09-20 02:35:10 +0100

    Final Index
    Commit the final index.

--- html/js/custom.js
+++ html/js/custom.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function($) {
 	//##########################################
 	
 	$("#tweets-bar").tweet({
-        count: 1,
-        username: "ansimuz"
+        count: 3,
+        username: "funnysugar"
     });
     
     
@@ -67,7 +67,7 @@ jQuery(document).ready(function($) {
 	$('#slider').nivoSlider({
 		effect: 'random', // Specify sets like: 'fold,fade,sliceDown'
 		animSpeed: 500, // Slide transition speed
-        pauseTime: 3000, // How long each slide will show
+        pauseTime: 30000, // How long each slide will show
         startSlide: 0, // Set starting Slide (0 index)
         controlNav: true // 1,2,3... navigation
 	});
